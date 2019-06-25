4845fc0196c6c873cfc2e62bb4df25d5543fdec0
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-25 21:44:42 +0000

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,42 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+ def get_character_movies_from_api(character_name)
+   #make the web request
+   response_string = RestClient.get('http://www.swapi.co/api/people/')
+   response_hash = JSON.parse(response_string)
+   films = response_hash["results"][0]["films"]
+   films.map do |url|
+     url_string = RestClient.get(url)
+     url_hash = JSON.parse(url_string)
+     url_hash["title"]
+   end
+ # iterate over the response hash to find the collection of `films` for the given
+ #   `character`
+ # collect those film API urls, make a web request to each URL to get the info
+ #  for that film
+ # return value of this method should be collection of info about each film.
+ #  i.e. an array of hashes in which each hash reps a given film
+ # this collection will be the argument given to `print_movies`
+ #  and that method will do some nice presentation stuff like puts out a list
+ #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+ films.each_with_index do |film, i|
+ puts "#{i + 1}. #{film}"
+
+ end
 end
 
 def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
+ films = get_character_movies_from_api(character)
+ print_movies(films)
 end
 
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
